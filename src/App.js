b2db0df0ce1ebd6a7f65d115dd25d9f57093be8f
abelelol <abeleamsalu@gmail.com>
2018-02-26 00:13:23 -0500

    added search and balance

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-//this is a comment
-const btcs = new WebSocket("wss://ws.blockchain.info/inv");
+import Search from './Search.js';
+import axios from 'axios'
 
-btcs.onopen = function(){
-	btcs.send(JSON.stringify({"op":"addr_unsub", "addr":"1N6d7Y6qMtr1B3NRCk3Ge2CDBgf4iLfzJR"}));
-};
+//this is a comment
 
 
 
@@ -15,25 +13,38 @@ class App extends Component {
   constructor(props){
 	  super(props)
 	  this.state = {
-		  btcs : btcs,
-		  balance: 0
+		  balance: 0,
+		  btcAddress: "1N6d7Y6qMtr1B3NRCk3Ge2CDBgf4iLfzJR",
+		  changed: false
 	  }
 
-	  btcs.onmessage = function(onmsg){
-	  	var response = JSON.parse(onmsg.data);
-	  	var getOuts = response.x.out;
-	  	var countOuts = getOuts.length;
-	  	var i;
-	  	for(i=0; i<countOuts; i++){
-	  		var outAdd = response.x.out[i].addr;
-	  		var address = "1N6d7Y6qMtr1B3NRCk3Ge2CDBgf4iLfzJR";
-	  		if(outAdd === address){
-	  			var amount = response.x.out[i].value;
-	  			this.updateBalance(amount)
-	  		};
-	    };
-	}
+	  this.getBalance = this.getBalance.bind(this)
 	  this.updateBalance = this.updateBalance.bind(this)
+	  this.connectToWallet = this.connectToWallet.bind(this)
+	  this.handleChange = this.handleChange.bind(this)
+	  this.handleSubmit = this.handleSubmit.bind(this)
+
+  }
+
+   componentDidMount(){
+
+	   this.setState({
+		   btcs: this.connectToWallet(this.state.btcAddress)
+	   })
+
+  }
+
+   handleChange(event) {
+     this.setState({btcAddress: event.target.value })
+   }
+
+   handleSubmit(event) {
+    event.preventDefault()
+	this.setState({
+		btcs: this.connectToWallet(this.state.btcAddress),
+		changed: true
+	})
+
   }
 
   updateBalance(amount){
@@ -42,6 +53,43 @@ class App extends Component {
 			  balance: amount/100000000
 		  }
 	  )
+  }
+
+  getBalance(address){
+	  let amount = 0
+	  axios.get('https://blockchain.info/q/addressbalance/'+this.state.btcAddress)
+	  		.then((response) =>{
+				this.updateBalance(response.data)
+			})
+			.catch(function(error){
+				console.log(error)
+			})
+  }
+
+  connectToWallet(address){
+	  const btcs = new WebSocket("wss://ws.blockchain.info/inv");
+
+	  btcs.onopen = function(){
+	  	btcs.send(JSON.stringify({"op":"addr_unsub", "addr": address}));
+	  };
+
+	  btcs.onmessage = function(onmsg){
+	  	var response = JSON.parse(onmsg.data);
+	  	var getOuts = response.x.out;
+	  	var countOuts = getOuts.length;
+	  	var i;
+	  	for(i=0; i<countOuts; i++){
+	  		var outAdd = response.x.out[i].addr;
+	  		if(outAdd === this.state.btcAddress){
+	  			var amount = response.x.out[i].value;
+	  			this.updateBalance(amount)
+	  		};
+	    };
+	}
+
+	this.getBalance(address)
+	return btcs
+
   }
 
 
@@ -54,15 +102,24 @@ class App extends Component {
           <h1 className="App-title">BlockChain App</h1>
 
         </header>
+		<Search
+			handleSubmit={this.handleSubmit}
+			handleChange={this.handleChange}
+			/>
         <p className="App-intro">
-        </p>
+		<p>
+			Bitcoin Wallet Address: {this.state.btcAddress}
+			<br />
+            Balance: {this.state.balance} BTC
+			<br />
+			changed: {"" + this.state.changed}
+		</p>
+		</p>
 		<p id="websocket">
 		{
-			this.state.balance == 0 ?
+			this.state.balance === 0 ?
 				"Recieved:BTC":
 				"Recieve" + this.state.balance + "BTC"
-
-
 		}
 		</p>
       </div>
--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
-const Search = ({ onChange }) => (
-  <input
+const Search = ({handleSubmit, handleChange }) => (
+  <form onSubmit={handleSubmit}>
+  	<input
     type="text"
-    onChange={onChange}
+    onChange={handleChange}
     placeholder="Enter Bitcoin address"
-  />
+  	/>
+	</form>
 )
 
 export default Search
